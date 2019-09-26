beb5b54282d015aceae77e93d1f01de3f8004454
SARANCVS <sarancvsl@gmail.com>
2019-09-26 15:42:20 +0530

    finished

---
+++ src/Abu.java
@@ -0,0 +1,26 @@
+
+public class Abu {
+
+	public void land() {
+		System.out.println("x510u");
+	}
+
+	public void house(int id) {
+
+	}
+	
+	public void house(String name) {
+		
+	}
+	
+	public void house( boolean b) {
+		
+	}
+
+	public static void main(String[] args) {
+
+		Abu a = new Abu();
+		a.land();
+		a.house(5);
+	}
+}
---
+++ src/Mama.java
@@ -0,0 +1,16 @@
+
+public class Mama extends Abu {
+	
+	public void desktopSite() {
+		System.out.println("desktop.in");
+	}
+
+	public static void main(String[] args) {
+		Mama b = new Mama();
+		b.desktopSite();
+		b.land();
+		b.house("velachery");
+		
+	}
+
+}
